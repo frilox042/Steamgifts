571afadf157088275679193adebf18760c2f9a50
getherdami <getherdami@eisti.eu>
2016-03-04 18:47:09 +0100

    Update: sg_requests.py

--- sg_requests.py
+++ sg_requests.py
@@ -6,9 +6,16 @@ import sqlite3
 import re
 import time
 import logging
+from sgdb import SgDb
+from functools import partial
 
 
 logger = logging.getLogger('sg_logger.sg_requests')
+
+
+class InvalidCookieException(Exception):
+    pass
+
 
 class SgRequests(object):
     """ A simple class using requests on steamgifts
@@ -24,6 +31,7 @@ class SgRequests(object):
     """
     base_url = 'http://www.steamgifts.com'
     sqlite_db_name = 'sg_entry.db'
+    timeout = (30.0, 30.0)  # (timeout, time to read)
     time_to_sleep_between_each_request = 0
     header_pages = {
         'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0'
@@ -47,24 +55,13 @@ class SgRequests(object):
         text = f.read()
         self.cookie = {'PHPSESSID': str(text).strip()}
         f.close()
+        try:
+            self.xsrf_token = self.__get_xsrf_token()
+        except:
+            logger.error("Invalid cookie")
+            raise InvalidCookieException()
         self.points = self.__get_current_points()
-        self.xsrf_token = self.__get_xsrf_token()
-
-    def __check_if_already_entered(self, giveaway_code):
-        conn = sqlite3.connect(self.sqlite_db_name)
-        c = conn.cursor()
-        c.execute("SELECT count(*) FROM ENTRY WHERE CODE= (?)", (giveaway_code,))
-        res = c.fetchone()[0] == 1
-        conn.close()
-        return res
-
-    def __add_an_entry(self, giveaway_code, giveaway_price, giveaway_link):
-        conn = sqlite3.connect(self.sqlite_db_name)
-        c = conn.cursor()
-        name = giveaway_link.split('/')[3]
-        c.execute("INSERT INTO ENTRY VALUES(?,?,?)", (giveaway_code, giveaway_price, name))
-        conn.commit()
-        conn.close()
+        self.db = SgDb(self.sqlite_db_name)
 
     def __get_xsrf_token(self):
         r = self.get('')
@@ -80,19 +77,17 @@ class SgRequests(object):
     def __getLink(self, soup):
         return soup.find('a', {'class': 'giveaway__heading__name'}).attrs['href']
 
-
     def __getCode(self, soup):
         link = self.__getLink(soup)
         return link.split('/')[2]
 
-
     def __getPrice(self, soup):
         return int(soup.findAll('span', {'class': 'giveaway__heading__thin'})[-1].text[1:-2])
 
-
     def get(self, path_url):
         time.sleep(self.time_to_sleep_between_each_request)
-        return requests.get(self.base_url + path_url, headers=self.header_pages, cookies=self.cookie)
+        logger.info('getting page : "' + self.base_url + path_url + '"')
+        return requests.get(self.base_url + path_url, headers=self.header_pages, cookies=self.cookie, timeout=self.timeout)
 
     def enter(self, giveaway_link, giveaway_code, giveaway_price):
         """ Send a request to enter to the giveaway
@@ -105,7 +100,7 @@ class SgRequests(object):
         :param giveaway_code: code of the giveaway
         :param giveaway_price: int value of giveaway
         """
-        if not self.__check_if_already_entered(giveaway_code):
+        if not self.db.check_if_already_entered(giveaway_code):
             if giveaway_price <= self.points:
                 time.sleep(self.time_to_sleep_between_each_request)
                 payload = {'xsrf_token': self.xsrf_token,
@@ -113,22 +108,25 @@ class SgRequests(object):
                 header_entry = self.header_entry_template
                 header_entry['Referer'] = self.base_url + giveaway_link
                 r = requests.post(self.base_url + "/ajax.php",
-                                  headers=header_entry, cookies=self.cookie, data=payload)
+                                  headers=header_entry, cookies=self.cookie, data=payload, timeout=self.timeout)
                 if r.status_code == 200:
                     json_tmp = json.loads(r.text)
                     if json_tmp['type'] == 'success':
                         self.points = int(json_tmp['points'])
-                        self.__add_an_entry(giveaway_code, giveaway_price, giveaway_link)
+                        self.db.add_an_entry(
+                            giveaway_code, giveaway_price, giveaway_link)
                         logger.info('Adding entry : ' + giveaway_code)
                     else:
                         logger.warning('Not a succes for : ' + giveaway_code)
                 else:
-                    logger.warning('Failed with status_code : ' + str(r.status_code))
+                    logger.warning(
+                        'Failed with status_code : ' + str(r.status_code))
             else:
                 logger.info('Not enough points to enter : ' +
-                      giveaway_link + ' / cost : ' + str(giveaway_price))
+                            giveaway_link + ' / cost : ' + str(giveaway_price))
         else:
             logger.info('Already Done for : ' + giveaway_code)
+
     def __page_has_next(self, soup):
         tmp = soup.findAll('a')
         return len([x for x in tmp if x.has_attr('data-page-number')]) != 0
@@ -139,77 +137,69 @@ class SgRequests(object):
         else:
             return False
 
-    def browser_pages(self, path_url, func, acc=[], page=1, limitPoint=-1):
-        """ Browser page of steamgifts beginning with page 1
-        For each call :param func: with soup of the request as first param
-        and add the result of func to the result in acc
-        Return list of tuple where tuple are (link, code, price)
-        """
+    def __generateEntry(self, soup):
+        page_heading = soup.find('div', {'class': 'page__heading'})
+        soup_wishlist = page_heading.next_sibling.next_sibling
+        div = soup_wishlist.find_all(
+            'div', {'class': 'giveaway__row-outer-wrap'})
+        for i in div:
+            yield (self.__getLink(i), self.__getCode(i), self.__getPrice(i))
+
+    def __generateEntry50copies(self, soup):
+        pinned = soup.find('div', {'class': 'pinned-giveaways__outer-wrap'})
+        div = pinned.find_all(
+            'div', {'class': 'giveaway__row-outer-wrap'})
+        for i in div:
+            yield (self.__getLink(i), self.__getCode(i), self.__getPrice(i))
+
+    def generateEntryByBrowsingPage(self, path_url, func, page=1):
         r = self.get(path_url + '&page=' + str(page))
         soup = bs(r.text, 'html.parser')
-        acc = acc + func(soup)
-        if (not self.__isLimitReached(limitPoint, acc)) and self.__page_has_next(soup):
-            return self.browser_pages(path_url, func, acc=acc, page=(page + 1), limitPoint=limitPoint)
-        else:
-            return acc
+        yield from func(soup)
+        if self.__page_has_next(soup):
+            yield from self.generateEntryByBrowsingPage(path_url, func, page=(page + 1))
 
-    def findAllWishListedGiveaway(self, limitPoint=-1):
-        """ Browser wishlist to find all giveaways in
-        Return list of tuple where tuple are (link, code, price)
-        """
-        def f(soup):
-            page_heading = soup.find('div', {'class': 'page__heading'})
-            soup_wishlist = page_heading.next_sibling.next_sibling
-            div = soup_wishlist.find_all(
-                'div', {'class': 'giveaway__row-outer-wrap'})
-            return [(self.__getLink(i), self.__getCode(i), self.__getPrice(i)) for i in div]
-        return self.browser_pages('/giveaways/search?type=wishlist', f, limitPoint=limitPoint)
+    def generateWishlistEntry(self):
+        yield from self.generateEntryByBrowsingPage('/giveaways/search?type=wishlist', self.__generateEntry)
 
-    def findAll50copies(self, limitPoint=-1):
-        """ Go to a page with only giveaway with more than 50 copies
-        Return list of tuple where tuple are (link, code, price)
-        """
-        def f(soup):
-            pinned = soup.find('div' , {'class' : 'pinned-giveaways__outer-wrap'})
-            div = pinned.find_all(
-                'div', {'class': 'giveaway__row-outer-wrap'})
-            return [(self.__getLink(i), self.__getCode(i), self.__getPrice(i)) for i in div]
-        return self.browser_pages('/giveaways/search?q=SthNeverMatchHere', f, limitPoint=limitPoint)
+    def generate50copiesEntry(self):
+        yield from self.generateEntryByBrowsingPage('/giveaways/search?q=SthNeverMatchHere', self.__generateEntry50copies)
 
-    def findAllByName(self, name, limitPoint=-1):
-        """ Find all giveaways mathing with name
-        Return list of tuple where tuple are (link, code, price)
-        """
-        def f(soup):
-            page_heading = soup.find('div', {'class': 'page__heading'})
-            soup_wishlist = page_heading.next_sibling.next_sibling
-            div = soup_wishlist.find_all(
-                'div', {'class': 'giveaway__row-outer-wrap'})
-            return [(self.__getLink(i), self.__getCode(i), self.__getPrice(i)) for i in div]
-        return self.browser_pages('/giveaways/search?q='+name, f, limitPoint=limitPoint)
+    def generateAllEntry(self):
+        yield from self.generateEntryByBrowsingPage('/giveaways/search?', self.__generateEntry)
 
-    def findAllGiveaway(self, limitPoint=300):
-        """ Find all giveaway
-        Return list of tuple where tuple are (link, code, price)
-        """
-        def f(soup):
-            page_heading = soup.find('div', {'class': 'page__heading'})
-            soup_wishlist = page_heading.next_sibling.next_sibling
-            div = soup_wishlist.find_all(
-                'div', {'class': 'giveaway__row-outer-wrap'})
-            return [(self.__getLink(i), self.__getCode(i), self.__getPrice(i)) for i in div]
-        return self.browser_pages('/giveaways/search?', f, limitPoint=limitPoint)
+    def generateEntryByName(self, name):
+        yield from self.generateEntryByBrowsingPage('/giveaways/search?q=' + name, self.__generateEntry)
 
 
-    def doEntry(self, giveaways):
-        """ Do entry for a list of tuple where tuple are (link, code, price)"""
-        self.points = self.__get_current_points()
-        for g in giveaways:
+    def pointGreaterThan(self, point):
+        return self.points > point
+
+    def doEntryByGenerator(self, entryGenerator, canContinue):
+        for g in entryGenerator:
+            if not canContinue():
+                break
             self.enter(*g)
 
     def run(self):
-        """ Simple way to enter wishlist and game with more than 50 copies"""
-        wishlist = self.findAllWishListedGiveaway()
-        self.doEntry(wishlist)
-        pinned = self.findAll50copies()
-        self.doEntry(pinned)
+        self.doEntryByGenerator(self.generateWishlistEntry(), partial(self.pointGreaterThan, point=0))
+        self.doEntryByGenerator(self.generate50copiesEntry(), partial(self.pointGreaterThan, point=0))
+        self.doEntryByGenerator(self.generateAllEntry(),
+                                partial(self.pointGreaterThan, point=0))
+
+    def sync(self):
+        header = self.header_entry_template
+        header['Referer'] = "http://www.steamgifts.com/account/profile/sync"
+        payload = {"xsrf_token": self.xsrf_token,
+                   "do": "sync"}
+        r = requests.post(self.base_url + "/ajax.php",
+                          cookies=self.cookie, headers=header, data=payload)
+        if r.status_code == 200:
+            json_tmp = json.loads(r.text)
+            if json_tmp['type'] == 'success':
+                logger.info(json_tmp["msg"])
+            else:
+                logger.warning("Fail to sync account")
+        else:
+            logger.warning(
+                'Failed to sync with status_code : ' + str(r.status_code))
